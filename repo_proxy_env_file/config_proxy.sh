02cd0d1f353048719ca219293869de45753f5e1f
glayneon <yeraongon@gmail.com>
2016-04-22 15:28:37 +0900

    Changes to be committed: 	modified:   repo_proxy_env_file/config_proxy.sh

--- repo_proxy_env_file/config_proxy.sh
+++ repo_proxy_env_file/config_proxy.sh
@@ -26,10 +26,13 @@ fi
 # check linux distribution type
 banner "Checking linux distribution type.."
 if [ "${VERSION_ID}" == "14.04" ]; then
-	UBUNTU_OK=true
+	UBUNTU4_OK=true
+	CENTOS_OK=false
+elif [ "${VERSION_ID}" == "16.04" ]; then
+	UBUNTU6_OK=true
 	CENTOS_OK=false
 elif [ "${VERSION_ID}" == "7" ]; then
-	UBUNTU_OK=false
+	UBUNTU4_OK=false
 	CENTOS_OK=true
 else
     fail "Only support CentOS7 & Ubuntu-14.04"
@@ -112,8 +115,8 @@ _EOF_
 	banner "Complete ${ID} Proxy repositories.."
 	banner "Please execute 'sudo yum clean all' to remove old repositories's metadata"
 
-# setting for ubuntu proxy
-elif [ "$UBUNTU_OK" == true ]; then
+# setting for ubuntu4 proxy
+elif [ "${UBUNTU4_OK}" == true ]; then
 	[ -e ${UBUNTU_REPO} ] && mv ${UBUNTU_REPO} ${UBUNTU_REPO%/*/*}00proxy.bak && banner "Backing up original file ${UBUNTU_REPO} to ${UBUNTU_REPO%/*/*}00proxy.bak"
 	[ -e ${UBUNTU_SRC_FILE} ] && mv ${UBUNTU_SRC_FILE} ${UBUNTU_SRC_FILE%list}bak && banner "Backing up original file ${UBUNTU_SRC_FILE} to ${UBUNTU_SRC_FILE%list}bak"
 	cat <<_EOF_ > "$UBUNTU_REPO"
@@ -183,4 +186,72 @@ _EOF_
 	banner "Complete ${ID} Proxy repositories.."
 	banner "Please execute 'sudo apt-get clean' to remove old repositories's metadata"
 
+
+# setting for ubuntu6 proxy
+elif [ "${UBUNTU6_OK}" == true ]; then
+	[ -e ${UBUNTU_REPO} ] && mv ${UBUNTU_REPO} ${UBUNTU_REPO%/*/*}00proxy.bak && banner "Backing up original file ${UBUNTU_REPO} to ${UBUNTU_REPO%/*/*}00proxy.bak"
+	[ -e ${UBUNTU_SRC_FILE} ] && mv ${UBUNTU_SRC_FILE} ${UBUNTU_SRC_FILE%list}bak && banner "Backing up original file ${UBUNTU_SRC_FILE} to ${UBUNTU_SRC_FILE%list}bak"
+	cat <<_EOF_ > "$UBUNTU_REPO"
+Acquire::http::Proxy "http://repos.atto.io:9999";
+Acquire::https::Proxy "http://repos.atto.io:9999";
+Acquire::ftp::Proxy "http://repos.atto.io:9999";
+_EOF_
+
+	cat <<_EOF_ > "$UBUNTU_SRC_FILE"
+#deb cdrom:[Ubuntu 16.04 LTS _Xenial Xerus_ - Release amd64 (20160420.1)]/ xenial main restricted
+
+# See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
+# newer versions of the distribution.
+deb http://ftp.daumkakao.com/ubuntu/ xenial main restricted
+# deb-src http://ftp.daumkakao.com/ubuntu/ xenial main restricted
+
+## Major bug fix updates produced after the final release of the
+## distribution.
+deb http://ftp.daumkakao.com/ubuntu/ xenial-updates main restricted
+# deb-src http://ftp.daumkakao.com/ubuntu/ xenial-updates main restricted
+
+## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
+## team, and may not be under a free licence. Please satisfy yourself as to
+## your rights to use the software. Also, please note that software in
+## universe WILL NOT receive any review or updates from the Ubuntu security
+## team.
+deb http://ftp.daumkakao.com/ubuntu/ xenial universe
+# deb-src http://ftp.daumkakao.com/ubuntu/ xenial universe
+deb http://ftp.daumkakao.com/ubuntu/ xenial-updates universe
+# deb-src http://ftp.daumkakao.com/ubuntu/ xenial-updates universe
+
+## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu 
+## team, and may not be under a free licence. Please satisfy yourself as to 
+## your rights to use the software. Also, please note that software in 
+## multiverse WILL NOT receive any review or updates from the Ubuntu
+## security team.
+deb http://ftp.daumkakao.com/ubuntu/ xenial multiverse
+# deb-src http://ftp.daumkakao.com/ubuntu/ xenial multiverse
+deb http://ftp.daumkakao.com/ubuntu/ xenial-updates multiverse
+# deb-src http://ftp.daumkakao.com/ubuntu/ xenial-updates multiverse
+
+## N.B. software from this repository may not have been tested as
+## extensively as that contained in the main release, although it includes
+## newer versions of some applications which may provide useful features.
+## Also, please note that software in backports WILL NOT receive any review
+## or updates from the Ubuntu security team.
+deb http://ftp.daumkakao.com/ubuntu/ xenial-backports main restricted universe multiverse
+# deb-src http://ftp.daumkakao.com/ubuntu/ xenial-backports main restricted universe multiverse
+
+## Uncomment the following two lines to add software from Canonical's
+## 'partner' repository.
+## This software is not part of Ubuntu, but is offered by Canonical and the
+## respective vendors as a service to Ubuntu users.
+# deb http://archive.canonical.com/ubuntu xenial partner
+# deb-src http://archive.canonical.com/ubuntu xenial partner
+
+deb http://ftp.daumkakao.com/ubuntu xenial-security main restricted
+# deb-src http://ftp.daumkakao.com/ubuntu xenial-security main restricted
+deb http://ftp.daumkakao.com/ubuntu xenial-security universe
+# deb-src http://ftp.daumkakao.com/ubuntu xenial-security universe
+deb http://ftp.daumkakao.com/ubuntu xenial-security multiverse
+# deb-src http://ftp.daumkakao.com/ubuntu xenial-security multiverse
+_EOF_
+	banner "Complete ${ID} Proxy repositories.."
+	banner "Please execute 'sudo apt-get clean' to remove old repositories's metadata"
 fi
